121046238f50f9dd4877d909a6121539214b7e7f
AMreyleen <andrusha26072004@gmail.com>
2025-08-28 19:34:52 +0300

    Made main pages of app

---
+++ ServiceCRM/Views/Settings/ChangePassword.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServiceCRM.Views.Settings
+{
+    public class ChangePasswordModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
