185b1d055b9bb1add9a6091a09854cf886d89822
Art <shai.ru1@yandex.ru>
2025-09-22 21:14:49 +0300

    Fix _Layout

--- ServiceCRM/obj/Debug/net8.0/ServiceCRM.AssemblyInfo.cs
+++ ServiceCRM/obj/Debug/net8.0/ServiceCRM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServiceCRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7283ad41b77d75ae93047dcda79e4d6c40740ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+765c588a6de16175a02ef4cd6bd39e8bb6c7d203")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServiceCRM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServiceCRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
